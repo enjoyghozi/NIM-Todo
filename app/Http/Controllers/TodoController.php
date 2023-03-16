7cf302e473ad3b9fe5273724635503b1d39ab154
arifbudimanar <arifbudiman01041996@hotmail.com>
2023-03-16 17:40:32 +0700

    create, read, search data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -9,13 +9,43 @@ class TodoController extends Controller
 {
     public function index()
     {
-        $todos = Todo::where('user_id', auth()->user()->id)->get();
-        dd($todos);
-        return view('todo.index');
+        $todos = Todo::where('user_id', auth()->user()->id)
+            ->orderBy('is_complete', 'asc')
+            ->orderBy('created_at', 'desc')
+            ->get();
+        // dd($todos);
+        return view('todo.index', compact('todos'));
     }
     public function create()
     {
         return view('todo.create');
+    }
+    public function store(Request $request, Todo $todo)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        // Practical
+        // $todo = new Todo;
+        // $todo->title = $request->title;
+        // $todo->user_id = auth()->user()->id;
+        // $todo->save();
+
+        // Query Builder way
+        // DB::table('todos')->insert([
+        //     'title' => $request->title,
+        //     'user_id' => auth()->user()->id,
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
+
+        // Eloquent Way - Readable
+        $todo = Todo::create([
+            'title' => ucfirst($request->title),
+            'user_id' => auth()->user()->id,
+        ]);
+        return redirect()->route('todo.index')->with('success', 'Todo created successfully!');
     }
     public function edit()
     {
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index(){
-        return view('user.index');
+    public function index()
+    {
+        $search = request('search');
+        if ($search) {
+            $users = User::where(function ($query) use ($search) {
+                $query->where('name', 'like', '%' . $search . '%')
+                    ->orWhere('email', 'like', '%' . $search . '%');
+            })
+                ->orderBy('name')
+                ->where('id', '!=', '1')
+                ->paginate(20)
+                ->withQueryString();
+        } else {
+            $users = User::where('id', '!=', '1')
+                ->orderBy('name')
+                ->paginate(10);
+        }
+        return view('user.index', compact('users'));
     }
 }
