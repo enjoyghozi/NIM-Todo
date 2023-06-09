cdb684875114c8bb32addafe77729012b593fd64
Ghosi Adam <adamghosi03@gmail.com>
2023-06-09 15:01:02 +0700

    clone project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,8 +28,8 @@ class DatabaseSeeder extends Seeder
         );
         User::create(
             [
-                'name' => 'Arif Budiman Arrosyid',
-                'email' => 'arifbudimanarrosyid@gmail.com',
+                'name' => 'Ghosi Adam',
+                'email' => 'adamghosi02@gmail.com',
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(10),
